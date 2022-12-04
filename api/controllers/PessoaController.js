0658d2a29a3da92a8289861496e4d9f96599977a
Eloan Ferreira <98027423+emffor@users.noreply.github.com>
2022-12-04 17:51:26 -0300

    Aula 04 - Atividade 05 - metodo atualizar e deletar

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -4,7 +4,7 @@ class PessoaController {
   // Método que retorna todas as pessoas cadastradas no banco de dados
   static async pegaTodasAsPessoas(req, res) {
     try {
-      const todasAsPessoas = await database.Pessoas.findAll(); // Faz a busca no banco de dados //serviço de banco de dados
+      const todasAsPessoas = await database.Pessoas.findAll(); // Faz a busca no banco de dados (serviço de banco de dados)
       return res.status(200).json(todasAsPessoas); // Retorna o resultado da busca
     } catch (error) {
       return res.status(500).json(error.message); // Retorna o erro
@@ -30,6 +30,32 @@ class PessoaController {
     try {
       const novaPessoaCriada = await database.Pessoas.create(novaPessoa); // Faz a criação no banco de dados
       return res.status(200).json(novaPessoaCriada); // Retorna o resultado da criação
+    } catch (error) {
+      return res.status(500).json(error.message); // Retorna o erro
+    }
+  }
+
+  // Método que atualiza uma pessoa pode ser atualizado todos os itens e apenas um item
+  static async atualizaPessoa(req, res) {
+    const { id } = req.params; // Pega o id da pessoa que está sendo atualizada
+    const novasInfos = req.body; // Pega o corpo da requisição e armazena na variável novasInfos { nome: "João", ativo: true, email: "
+    try {
+      await database.Pessoas.update(novasInfos, { where: { id: Number(id) } }); // Faz a atualização no banco de dados, passando as novas informações e o id da pessoa que será atualizada
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      }); // Faz a busca no banco de dados para retornar a pessoa atualizada
+      return res.status(200).json(pessoaAtualizada); // Retorna o resultado da atualização
+    } catch (error) {
+      return res.status(500).json(error.message); // Retorna o erro
+    }
+  }
+
+  // Método que deleta uma pessoa
+  static async apagaPessoa(req, res) {
+    const { id } = req.params; // Pega o id da pessoa que será deletada
+    try {
+      await database.Pessoas.destroy({ where: { id: Number(id) } }); // Faz a deleção no banco de dados
+      return res.status(200).json({ mensagem: `id ${id} deletado` }); // Retorna o resultado da deleção
     } catch (error) {
       return res.status(500).json(error.message); // Retorna o erro
     }
--- api/routes/pessoasRoutes.js
+++ api/routes/pessoasRoutes.js
@@ -6,5 +6,7 @@ const router = Router(); // Inicializa o Router
 router.get("/pessoas", PessoaController.pegaTodasAsPessoas); // Define a rota e o método que será chamado
 router.get("/pessoas/:id", PessoaController.pegaUmaPessoa); // Define a rota e o método que será chamado
 router.post("/pessoas", PessoaController.criaPessoa); // Define a rota e o método que será chamado
+router.put("/pessoas/:id", PessoaController.atualizaPessoa); // Define a rota e o método que será chamado
+router.delete("/pessoas/:id", PessoaController.apagaPessoa); // Define a rota e o método que será chamado
 
 module.exports = router; // Exporta o router
